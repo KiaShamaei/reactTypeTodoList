bea1043ceb988c8b0836c47e584e8142deda9c22
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2022-02-16 10:43:30 +0330

    initial part

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,52 @@
+import React, { ChangeEvent, FormEvent } from "react";
+
+type AddTodoProps = {
+  task: string;
+  handleChangeText: (e: ChangeEvent) => void;
+  handleChangePriority: (e: ChangeEvent) => void;
+  handleSubmit: (e: FormEvent) => void;
+};
+
+export const AddTodo = ({ task, handleChangeText , handleChangePriority , handleSubmit }: AddTodoProps) => {
+  return (
+    <form
+      className="w-12/12 flex flex-col justify-center items-center mb-4"
+      onSubmit={handleSubmit}
+    >
+      <div className="w-8/12 m-2 ">
+      <label>Task</label>
+      <input
+        className=" w-12/12 flex rounded shadow p-2 text-gray-700 "
+        type="text"
+        name="task"
+        placeholder="Add todo..."
+        value={task}
+        onChange={handleChangeText}
+      />
+      </div>
+      <div className="w-8/12 m-2 " >
+      <label>Priority</label>
+       <input
+        className=" w-12/12 flex rounded shadow p-2 text-gray-700 "
+        type="text"
+        name="priority"
+        placeholder="priority 1-5..."
+        value={task}
+        onChange={handleChangePriority}
+      />
+      </div>
+   
+  
+      <button
+        className={`
+            flex justify-center items-center  text-lg font-bold rounded-3xl mt-5 m-1 p-3
+            ${task === "" ? "bg-gray-400" : "bg-green-500"}
+        `}
+        aria-label="A submit button"
+        type="submit"
+      >
+        Add Task
+      </button>
+    </form>
+  );
+};
---
+++ src/todos.ts
@@ -0,0 +1,20 @@
+export const data = [
+  {
+    id: "1",
+    task: "Create React app",
+    isCompleted: true,
+    priority : 2
+  },
+  {
+    id: "2",
+    task: "Install TailwindCSS",
+    isCompleted: true,
+    priority : 1
+  },
+  {
+    id: "3",
+    task: "Use TypeScript to create an app",
+    isCompleted: false,
+    priority : 3
+  },
+];
